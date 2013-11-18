8cce9c542e8e6c4a7b861a4f711b8335acfd6232
Witcher42 <dy.lengleng@gmail.com>
2013-11-19 00:24:02 +0800

    1. Add list template

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 
 var filepath = '/Users/witcher42/tmp/hosts',
     options = { encoding: 'utf8' },
-    hosts = '',
+    hosts = null,
+    list = null,
     $content;
 
 hosts = fs.readFileSync(filepath, options);
@@ -13,3 +14,53 @@ $('#js-list-hosts').on('click', function(e) {
 $content = $('#js-content');
 
 $content.text(hosts);
+
+hosts = [
+  {
+    name: 'Common',
+    host: '',
+    active: false,
+    toggle: false
+  },
+  {
+    name: 'QA',
+    host: '',
+    active: true,
+    toggle: true
+  },
+  {
+    name: 'Dev',
+    host: '',
+    active: false,
+    toggle: true
+  },
+];
+
+try {
+  hosts = JSON.parse(localStorage.getItem('hosts')) || hosts;
+} catch(e) {
+
+}
+
+var $item,
+    $template;
+
+hosts.forEach(function(element, index) {
+
+  $template = $($('#tpl-list').html());
+
+  if (element.active) {
+    $template.addClass('active');
+  }
+
+  if (element.toggle) {
+    $template.attr('data-toggle', 'tab');
+  }
+
+  $template.find('a')
+      .data('hosts', element.host)
+      .text(element.name);
+
+  $template.insertBefore('#tpl-list');
+
+});
