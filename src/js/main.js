af1ec877f74cd81f29bcccc75e7a16653cf30290
Witcher42 <dy.lengleng@gmail.com>
2013-11-29 23:01:22 +0800

    1. Add view, which handles DOM operation

--- src/js/main.js
+++ src/js/main.js
@@ -31,10 +31,88 @@ $doc = $(document);
 
 var $item,
     hosts,
-    source,
-    html = $('#tpl-list').html();
+    view,
+    source;
 
 source = require('./js/source');
+
+view = {
+
+  nav: $('#js-tpl-list').html(),
+  content: $('#js-tpl-content').html(),
+
+  init: function(hosts) {
+    hosts.forEach(function(element, index) {
+      view.add(element);
+    });
+  },
+
+  add: function(element) {
+    this._addNav(element);
+    this._addContent(element);
+  },
+
+  show: function(host) {
+  },
+
+  _addNav: function(element) {
+    var hashArr,
+        html,
+        $item;
+
+    hashArr = {
+      hostname: element.name,
+      contentname: element.name.toLowerCase()
+    };
+
+    html = this.nav;
+
+    $item = $(render(html, hashArr));
+
+    if (element.active) {
+      $item.addClass('active');
+    }
+
+    if (element.custom) {
+      $item.addClass('js-custom');
+    }
+
+    if (element.using) {
+      $item.addClass('using');
+    }
+
+    $item.data('hosts', element.host);
+
+    $item.insertBefore('#js-tpl-list');
+  },
+
+  _addContent: function(element) {
+    var hashArr,
+        html,
+        $item,
+        $content;
+
+    html = this.content;
+
+    hashArr = {
+      hostname: element.name.toLowerCase()
+    };
+
+    $item = $(render(html, hashArr));
+    $content = $item.find('textarea');
+    $content.val(element.host);
+    $item.insertBefore('#js-tpl-content');
+
+    CodeMirror.fromTextArea($content.get(0), { mode: 'shell' } );
+  },
+
+  mousetrap: function() {
+    Mousetrap.bind('command+s', function() {
+      $('.switcher-content.active textarea').
+    });
+  }
+
+};
 
 hosts = {
 
